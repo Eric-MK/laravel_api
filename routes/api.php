e68c57d01332112a149384ad3f2de6b900c9fd38
unknown <mutungaeric839@gmail.com>
2023-04-27 22:34:01 +0300

    more comments for more understanding

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();//returns authenticated router
+Route::middleware('auth:api')->get('/user', function (Request $request) {//$request header as the token 
+    return $request->user();//returns authenticated user information
 });
 
 Route::post('/login',[LoginController::class,'login']);
