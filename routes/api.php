deec6890ff45fea64ac214134b23ea2e98b97028
unknown <mutungaeric839@gmail.com>
2023-04-27 22:30:09 +0300

    works by generating the token

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();//returns authenticated router
 });
 
-Route::get('/login',[LoginController::class,'login']);
+Route::post('/login',[LoginController::class,'login']);
